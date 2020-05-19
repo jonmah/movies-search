587948fa5c59e8dd7432c522a8854b1e9144debd
Jon Mah <jonmah@MLT077569.local>
2020-05-18 22:22:05 -0700

    minimize some rendering in App

--- src/App.js
+++ src/App.js
@@ -29,21 +29,26 @@ const App = () => {
     })
   }
 
+  const renderErrorMessage = errorMessage =>
+    errorMessage && <div className='errorMessage'>{errorMessage}</div>
+  const renderLoading = loading => errorMessage =>
+    loading && !errorMessage && <span>loading...</span>
+  const renderMovies = A => loading => errorMessage =>
+    !loading &&
+    !errorMessage &&
+    A.map((movie, index) => (
+      <Movie key={`${index}-${movie.Title}`} movie={movie} />
+    ))
+
   return (
     <div className='App'>
       <Header text='HOOKED' />
       <Search search={search} />
       <p className='App-intro'>Sharing a few of our favourite movies</p>
       <div className='movies'>
-        {loading && !errorMessage ? (
-          <span>loading...</span>
-        ) : errorMessage ? (
-          <div className='errorMessage'>{errorMessage}</div>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
+        {renderLoading(loading)(errorMessage)}
+        {renderErrorMessage(errorMessage)}
+        {renderMovies(movies)(loading)(errorMessage)}
       </div>
     </div>
   )
